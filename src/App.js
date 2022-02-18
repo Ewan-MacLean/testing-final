05efb5f719bcde89a640f80d27d2bde4b6f1fcad
ewan-maclean <ewanmaclean77@gmail.com>
2022-02-18 15:34:16 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+import './components/functions/functions'
+import { countVowels, popup, reverseString, toRomanNumeral } from './components/functions/functions';
+
+// popup()
+
+reverseString('oink')
+countVowels('spiderman')
+toRomanNumeral(6)
+// returnRandomString()
+// uppercaseAll()
+// uppercaseSome()
+// addDomElement()
+// delDomElement()
+// animation()
+// changeBackgroundColor()
+// flipText()
 
 function App() {
   return (
--- src/components/functions/functions.js
+++ src/components/functions/functions.js
@@ -1,41 +1,78 @@
-function animation() {
+
+function popup() {
+    alert('OOOIIIIInkkkkkk')
+}
+
+function reverseString(str) {
+    console.log('reverseString on: ' + str)
+    const arr = str.split('')
+    // console.log(arr)
+    const revArr = arr.reverse()
+    // console.log(revArr)
+    console.log(revArr.join(''))
 
 }
 
-function reverseString() {
-    
+// try and write this with the map method
+function countVowels(str) {
+    const vowels = ['a','e','i','o','u']
+    let numVowels = 0
+    const strArr = str.split('')
+    for (let p=0;p<strArr.length; p++) {
+        for (let i=0;i<vowels.length;i++) {
+            if (strArr[p] == vowels[i]) {
+                numVowels = numVowels + 1
+            }
+        }
+    }
+    console.log('there are ' + numVowels + ' vowels in the string: ' + str)
 }
 
-function countVowels() {
+function toRomanNumeral(num) {
+    const index = {
+        I :1,
+        II :2,
+        III :3,
+        IV :4,
+        V :5,
+        VI :6,
+        VII :7,
+        VIII :8,
+        IX : 9,
+        X : 10
+    }
 
-}
+    for (key in index) {
+        result += key.repeat(Math.floor(num / index[key]));
+        num %= index[key];
+      }
 
-function toRomanNumeral() {
+    let result = ''
 
+    console.log(result)
 }
 
 function returnRandomString() {
-
+    
 }
 
 function uppercaseAll() {
-
+    
 }
 
 function uppercaseSome() {
-
-}
-
-function popup() {
-
+    
 }
 
 function addDomElement(){
-
+    
 }
 
 function delDomElement(){
+    
+}
 
+function animation() {
 }
 
 function changeBackgroundColor() {
@@ -44,6 +81,21 @@ function changeBackgroundColor() {
 
 function flipText() {
 
+}
+
+export {
+    popup,
+    reverseString,
+    countVowels,
+    toRomanNumeral
+    // returnRandomString,
+    // uppercaseAll,
+    // uppercaseSome,
+    // addDomElement,
+    // delDomElement, 
+    // animation,
+    // changeBackgroundColor,
+    // flipText
 }
 
 
