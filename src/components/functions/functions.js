3e5c0723327819ea081364c711ba11b68f8fac15
ewan-maclean <ewanmaclean77@gmail.com>
2022-02-18 00:41:25 -0400

    .

---
+++ src/components/functions/functions.js
@@ -0,0 +1,50 @@
+function animation() {
+
+}
+
+function reverseString() {
+    
+}
+
+function countVowels() {
+
+}
+
+function toRomanNumeral() {
+
+}
+
+function returnRandomString() {
+
+}
+
+function uppercaseAll() {
+
+}
+
+function uppercaseSome() {
+
+}
+
+function popup() {
+
+}
+
+function addDomElement(){
+
+}
+
+function delDomElement(){
+
+}
+
+function changeBackgroundColor() {
+
+}
+
+function flipText() {
+
+}
+
+
+
