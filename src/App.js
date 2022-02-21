ed6bd3839c2e78c3905d2be792969600dd3c17a2
ewan-maclean <ewanmaclean77@gmail.com>
2022-02-21 11:42:35 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import './components/functions/functions'
-import { countVowels, popup, reverseString, toRomanNumeral } from './components/functions/functions';
+import { addDomElement, changeBackgroundColor, countVowels, DOMFunctions, popup, returnRandomString, reverseString, toRomanNumeral, uppercaseAll, uppercaseSome } from './components/functions/functions';
 
 // popup()
 
 reverseString('oink')
 countVowels('spiderman')
-toRomanNumeral(6)
-// returnRandomString()
-// uppercaseAll()
-// uppercaseSome()
-// addDomElement()
+// toRomanNumeral(6)
+returnRandomString()
+uppercaseAll('this is a lowercase sentence?')
+uppercaseSome('hamburgers are a nutritious meal')
+addDomElement()
+
+function addNewDOMElement() {
+  const newElement = document.createElement('p')
+  const mainElement = (document.getElementById('main'))
+  newElement.innerText = 'im new here! '
+  // console.log(newElement)
+  mainElement.appendChild(newElement)
+}
+
 // delDomElement()
 // animation()
-// changeBackgroundColor()
 // flipText()
 
 function App() {
   return (
     <div className="App">
+      <div id='main'>
+
+      <button className='bgcolor-button' name='changeBG' onClick={changeBackgroundColor}>Change background color</button>
+      <button className='addDOM-button' name='addDOM' onClick={addNewDOMElement}>Add something!</button>
+      
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/functions/functions.js
+++ src/components/functions/functions.js
@@ -9,7 +9,7 @@ function reverseString(str) {
     // console.log(arr)
     const revArr = arr.reverse()
     // console.log(revArr)
-    console.log(revArr.join(''))
+    console.log('result: ' + (revArr.join('')))
 
 }
 
@@ -28,73 +28,123 @@ function countVowels(str) {
     console.log('there are ' + numVowels + ' vowels in the string: ' + str)
 }
 
-function toRomanNumeral(num) {
-    const index = {
-        I :1,
-        II :2,
-        III :3,
-        IV :4,
-        V :5,
-        VI :6,
-        VII :7,
-        VIII :8,
-        IX : 9,
-        X : 10
-    }
+// function toRomanNumeral(num) {
+//     const index = {
+//         I :1,
+//         II :2,
+//         III :3,
+//         IV :4,
+//         V :5,
+//         VI :6,
+//         VII :7,
+//         VIII :8,
+//         IX : 9,
+//         X : 10
+//     }
 
-    for (key in index) {
-        result += key.repeat(Math.floor(num / index[key]));
-        num %= index[key];
-      }
+//     for (key in index) {
+//         result += key.repeat(Math.floor(num / index[key]));
+//         num %= index[key];
+//       }
 
-    let result = ''
+//     let result = ''
 
-    console.log(result)
-}
+//     console.log(result)
+// }
 
 function returnRandomString() {
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    const numbers = [1,2,3,4,5,6,7,8,9,0]
     
+    const randomStrArr = []
+    for (let i=0;i<20;i++) {
+        let randomNumABC = Math.floor(Math.random() * 25)
+        let randomNum123 = Math.floor(Math.random() * 9)
+        let letterOrNum = (Math.random() < 0.5)
+        if (letterOrNum === true) {
+            randomStrArr.push(alphabet[randomNumABC])
+        }
+        else {
+            randomStrArr.push(numbers[randomNum123])
+        }
+    }
+    console.log(randomStrArr.join(''))
 }
 
-function uppercaseAll() {
-    
+function uppercaseAll(str) {
+    const myString = str.toUpperCase()
+    console.log(str)
+    console.log(myString)
 }
 
-function uppercaseSome() {
-    
+function uppercaseSome(str) {
+    console.log(str)
+    const strArr = str.split('')
+    const newStr = []
+    for (let i=0;i<strArr.length;i++) {
+        let x = (Math.floor(Math.random() * 2) == 0);
+        if(x){
+               newStr.push(strArr[i].toUpperCase())
+            }   
+        else {
+            newStr.push(strArr[i])
+        }
+    }
+    console.log(newStr.join(''))
+
 }
 
-function addDomElement(){
-    
+
+function addDomElement() {
+
+        document.addEventListener("DOMContentLoaded", function(event) {
+            // console.log("DOM fully loaded and parsed");
+            const newElement = document.createElement('p')
+            const mainElement = (document.getElementById('main'))
+            newElement.innerText = 'im new here! (first)'
+            mainElement.appendChild(newElement)
+        
+    });
 }
+
+
 
 function delDomElement(){
-    
 }
 
 function animation() {
+    
 }
 
 function changeBackgroundColor() {
-
+    console.log('changing background color....')
+    const body = document.querySelector('#body')
+    const colors = ["red","yellow","white","slateblue","green"]
+    body.style["background-color"] = colors[Math.floor(Math.random() * 4)]
 }
 
 function flipText() {
 
 }
 
+    // document is already ready to go
+// }
+
+
 export {
+    // isReady,
     popup,
     reverseString,
     countVowels,
-    toRomanNumeral
-    // returnRandomString,
-    // uppercaseAll,
-    // uppercaseSome,
-    // addDomElement,
+    // toRomanNumeral
+    returnRandomString,
+    uppercaseAll,
+    uppercaseSome,
+    // DOMFunctions,
+    addDomElement,
     // delDomElement, 
     // animation,
-    // changeBackgroundColor,
+    changeBackgroundColor
     // flipText
 }
 
